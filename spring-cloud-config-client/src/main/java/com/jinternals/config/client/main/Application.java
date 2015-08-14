4083011a5f0a713145b8dd1137f66ebf9c7a756c
Mradul Pandey <mradul@metadesignsolutions.in>
2015-08-14 09:59:37 +0530

    updated spring cloud version and applied security by encryption credential in client and server property files

--- spring-cloud-config-client/src/main/java/com/jinternals/config/client/main/Application.java
+++ spring-cloud-config-client/src/main/java/com/jinternals/config/client/main/Application.java
@@ -1,7 +1,5 @@
 package com.jinternals.config.client.main;
 
-import java.lang.reflect.Field;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +11,6 @@ import com.jinternals.config.client.context.ClientContext;
 @EnableAutoConfiguration
 @Import({ ClientContext.class })
 public class Application {
-
-	static {
-		try {
-			Class<?> cls = Class.forName("javax.crypto.JceSecurity");
-			Field field = cls.getDeclaredField("isRestricted");
-			field.setAccessible(true);
-			field.set(null, java.lang.Boolean.FALSE);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
 
 	public static void main(String[] args) {
 
