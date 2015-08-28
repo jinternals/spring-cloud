cba3990ca115010bf37108b0ce644f7d5c3c5df3
Mradul Pandey <mradul@metadesignsolutions.in>
2015-08-28 16:01:17 +0530

    added support for encrypted credentials

--- spring-cloud-config-server/src/main/java/com/jinternals/config/server/context/ConfigContext.java
+++ spring-cloud-config-server/src/main/java/com/jinternals/config/server/context/ConfigContext.java
@@ -15,11 +15,9 @@
  */
 package com.jinternals.config.server.context;
 
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigServer
 public class ConfigContext {
 
 }
